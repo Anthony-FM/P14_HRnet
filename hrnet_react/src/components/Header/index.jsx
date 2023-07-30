0e721c1c2ec013ccc3ff002a93e319bc24a2c8c7
Anthony Fouda Many <anthony.foudamany@gmail.com>
2023-07-30 19:02:31 +0200

    :fire: Ajout hook/action et location

--- hrnet_react/src/components/Header/index.jsx
+++ hrnet_react/src/components/Header/index.jsx
@@ -1,15 +1,29 @@
 import './index.css'
-//
+// react-router
 import { Link } from 'react-router-dom'
+// hook
+import {  useDispatch} from 'react-redux'
+// action
+import { toggleLink } from '../../features/header'
 
 export default function Header(){
+    const dispatch = useDispatch()
+
+    // Récupération du path de l'URL
+    let location = window.location.href
+    console.log(location.includes("EmployeesList"))
+
     return <header>
         <div>
-            <Link to="/" className='logo'>HRnet</Link>
+            <Link to="/" className='logo' onClick={() => dispatch(toggleLink(false))}>HRnet</Link>
         </div>
         <div className='linkContainer'>
-            <Link to="/CreateEmployee" className='link'>Create Employee</Link>
-            <Link to="/EmployeesList" className='link'>View Current Employees</Link>
+            { 
+                location.includes("CreateEmployee") ? 
+                    (<Link to="/EmployeesList" className='link' onClick={() => dispatch(toggleLink(false))}>View Current Employees</Link>) 
+                    : 
+                    (<Link to="/CreateEmployee" className='link' onClick={() => dispatch(toggleLink(true))}>Create Employee</Link>)
+            }     
         </div>
     </header>
 }
