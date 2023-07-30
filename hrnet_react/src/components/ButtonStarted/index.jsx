127d7f39128b8bca8a6cf6e39581721d844399a7
Anthony Fouda Many <anthony.foudamany@gmail.com>
2023-07-30 18:58:10 +0200

    Ajout action toggleLink

--- hrnet_react/src/components/ButtonStarted/index.jsx
+++ hrnet_react/src/components/ButtonStarted/index.jsx
@@ -1,8 +1,14 @@
 import { Link } from 'react-router-dom'
+//css
 import './index.css'
+// action
+import { toggleLink } from '../../features/header'
+// hook
+import { useDispatch } from 'react-redux'
 
 export default function ButtonStarted(){
+    const dispatch = useDispatch()
     return <div className='buttonStart'>
-        <Link to="/CreateEmployee" className='linkStart'>Let's started</Link>
+        <Link to="/CreateEmployee" className='linkStart' onClick={() => dispatch(toggleLink(true))}>Let's started</Link>
     </div>
 }
