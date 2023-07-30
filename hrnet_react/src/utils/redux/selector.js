b68faa36d27a884f78b15a04e1d628cad9cb1a26
Anthony Fouda Many <anthony.foudamany@gmail.com>
2023-07-30 19:55:11 +0200

    🔥 création des selector pour chaques slices

--- hrnet_react/src/utils/redux/selector.js
+++ hrnet_react/src/utils/redux/selector.js
@@ -1,1 +1,4 @@
 export const selectForm = (state) => state.form
+export const selectModal = (state) => state.modal
+export const selectHeader = (state) => state.header
+export const selectSearch = (state) => state.search
