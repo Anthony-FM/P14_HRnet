55c9482dbe90376e22c9caacca4ec0ae892f4cdc
Anthony Fouda Many <anthony.foudamany@gmail.com>
2023-07-30 19:52:28 +0200

    🔥 Ajout des slices au store

--- hrnet_react/src/utils/redux/store.js
+++ hrnet_react/src/utils/redux/store.js
@@ -1,10 +1,17 @@
 // feature
 import form from "../../features/form";
+import modal from "../../features/modal";
+import header from "../../features/header";
+import search from "../../features/search";
 // configureStore @reduxToolkit
 import { configureStore } from "@reduxjs/toolkit";
 
 export default configureStore({
     reducer: {
-        form: form
+        form: form,
+        modal : modal,
+        header : header,
+        search: search
+
     }
 })
