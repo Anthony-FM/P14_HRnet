5496a1c59356a8ff59ca9393778871c80d4b0981
Anthony Fouda Many <anthony.foudamany@gmail.com>
2023-07-30 19:48:38 +0200

    🗓️ Création des colonnes pour react-table et react-fns

---
+++ hrnet_react/src/utils/table/colums.js
@@ -0,0 +1,41 @@
+import { format } from 'date-fns'
+
+export const COLUMNS = [
+    {
+        Headers: "First Name",
+        accessor: "firstname",
+    }
+    ,{
+        Headers: "Last Name",
+        accessor: "lastname",
+    }
+
+    ,{
+        Headers: "Date Of Birth",
+        accessor: "dateOfBirth",
+        Cell: ({value}) => {  return value ? format(new Date(value), 'MM/dd/yyyy') : new Date("undefined").toISOString},
+    }
+
+    ,{
+        Headers: "Start Date",
+        accessor: "startDate",
+        Cell: ({value}) => { return value ? format(new Date(value), 'MM/dd/yyyy') : new Date("undefined").toISOString},
+    }
+
+    ,{
+        Headers: "Street",
+        accessor: "street",
+    }
+    ,{
+        Headers: "City",
+        accessor: "city",
+    }
+    ,{
+        Headers: "State",
+        accessor: "state",
+    }
+    ,{
+        Headers: "ZipCode",
+        accessor: "zipCode",
+    }
+]
